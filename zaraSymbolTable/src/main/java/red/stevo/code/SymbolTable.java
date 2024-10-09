102a374b5d2461598328c6f7ea4dc653b8896fb1
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-10-09 11:12:29 +0300

    Commit message >>>>> handle add new sybol to the symbol table, and entering a new scope <<<<<

--- zaraSymbolTable/src/main/java/red/stevo/code/SymbolTable.java
+++ zaraSymbolTable/src/main/java/red/stevo/code/SymbolTable.java
@@ -1,5 +1,39 @@
 package red.stevo.code;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+@Data
+@AllArgsConstructor
 public class SymbolTable {
 
+    private HashMap<String, Symbol> globalScope;
+
+    private Stack<Scope> localScopeStack;
+
+    private HashMap<String, Scope> scopeResolutionMap;
+
+
+    /*Enter into a new scope; whether class, method or block scope.\
+    * Create a new hashmap to store the symbols at that scope.*/
+    public void enterNewScope(String scopeType){
+        localScopeStack.push(new Scope(scopeType, new HashMap<>()));
+    }
+
+    /*Adds a new symbol to the current scope.The Current scope is the one at the peek of the stack.*/
+    public void addSymbol(String tokenVariableName, Object tokenValue, String tokenVariableType, String tokenScope) {
+
+        Symbol symbol = new Symbol(tokenVariableName, tokenValue, tokenVariableType, tokenScope);
+
+        if ("global".equals(tokenScope))
+            globalScope.put(tokenScope, symbol);
+        else {
+            localScopeStack.peek().getScopeTable().put(tokenVariableName, symbol);
+
+            scopeResolutionMap.put(tokenVariableName, localScopeStack.peek());
+        }
+    }
 }
