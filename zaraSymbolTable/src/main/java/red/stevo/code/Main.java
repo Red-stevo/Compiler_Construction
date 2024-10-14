4a5bab0878a039aab963dc21c3235f5fa88fb6a1
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-10-14 11:15:29 +0300

    Progressing

--- zaraSymbolTable/src/main/java/red/stevo/code/Main.java
+++ zaraSymbolTable/src/main/java/red/stevo/code/Main.java
@@ -1,11 +1,12 @@
 package red.stevo.code;
 
 
+import java.io.FileReader;
 import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
     }
 }
