e079e3068ad478588547e2738936b7ef449f0c30
mirowe <mirowe@localhost.localdomain>
2024-10-24 13:14:12 +0300

    ...

--- zaraSymbolTable/src/main/java/red/stevo/code/Main.java
+++ zaraSymbolTable/src/main/java/red/stevo/code/Main.java
@@ -4,8 +4,10 @@ package red.stevo.code;
 import red.stevo.code.Parser.ZaraLexer;
 import red.stevo.code.Parser.parser;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,5 +15,17 @@ public class Main {
         parser parser = new parser(lexer);
 
         System.out.println(parser.scan());
+
+    }
+
+    void topDownParser(){
+
+       /* InputStream input = new FileReader("your_input_file.txt");
+
+
+        ZaraParser parser = new ZaraParser(input);
+        parser.Start();  // Start parsing based on your grammar's start rule
+
+        System.out.println("Parsing completed successfully!");*/
     }
 }
